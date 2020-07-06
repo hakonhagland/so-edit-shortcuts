00e12e507c28b2c47b0690ef234f8c9a08afc67b
Håkon Hægland <hakon.hagland@gmail.com>
2020-07-06 13:36:17 +0200

    Update7

--- so_edit_shortcuts.user.js
+++ so_edit_shortcuts.user.js
@@ -31,7 +31,7 @@ var rootNode = $("#content");
 var scConfig = [
     // titleName,   tagText,  btnText,          bSoloTag, bNotTag, keyTxt, keyCode, kbModifiers (Alt/Ctrl/Shift), kbModArry, bWrapByWord
     // 0            1         2                 3      4      5          6   7        8   9
-    ["singleQuote",    "`",    "xx",  false, true, ",",       219, ["Ctrl"], [], true],
+    ["singleQuote",    "x",    "xx",  false, true, ",",       219, ["Ctrl"], [], true],
     ["tripleQuote",    "```",    "xx",  false, true, ".",       221, ["Ctrl"], [], true],
     ["textQuote",    "xxx",    "xx",  false, true, ".",       188, ["Ctrl"], [], true],
 ];
